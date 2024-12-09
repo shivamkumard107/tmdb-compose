2eeb48462fdb7b38056080de5391f5fd60d0116d
shivamkumard107 <shivam.kumard107@gmail.com>
2024-12-09 14:30:10 +0530

    add movie details screen

---
+++ app/src/main/java/com/dev/sk/compose/ui/base/moviedetails/MovieDetailsScreen.kt
@@ -0,0 +1,84 @@
+package com.dev.sk.compose.ui.base.moviedetails
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.dev.sk.compose.R
+import com.dev.sk.compose.repository.model.Movie
+import com.dev.sk.compose.utils.Constants
+
+@Composable
+fun MovieDetailsScreen(
+    navController: NavController,
+    movie: Movie
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            ),
+            onClick = {
+                navController.popBackStack()
+            },
+            content = {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.baseline_chevron_left_24
+                    ),
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier
+                .padding(top = 10.dp)
+        )
+
+        AsyncImage(
+            model = Constants.FETCH_IMAGE_BASE_URL + movie.posterPath,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, start = 40.dp, end = 40.dp)
+                .clip(RoundedCornerShape(15.dp)),
+            error = painterResource(id = R.drawable.img_not_available_200)
+        )
+
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.bodyLarge,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(bottom = 30.dp, top = 30.dp, start = 20.dp)
+        )
+
+        Text(
+            text = movie.overview,
+            style = MaterialTheme.typography.bodyMedium,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(horizontal = 20.dp)
+        )
+    }
+}
