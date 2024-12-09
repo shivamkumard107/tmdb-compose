e5256965b329abab9b893d1502aea275131febf2
shivamkumard107 <shivam.kumard107@gmail.com>
2024-12-09 14:30:42 +0530

    setup MainActivity navigation

--- app/src/main/java/com/dev/sk/compose/ui/MainActivity.kt
+++ app/src/main/java/com/dev/sk/compose/ui/MainActivity.kt
@@ -1,23 +1,61 @@
 package com.dev.sk.compose.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dev.sk.compose.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.dev.sk.compose.repository.model.Movie
+import com.dev.sk.compose.ui.base.CustomNavType
+import com.dev.sk.compose.ui.base.moviedetails.MovieDetailsScreen
+import com.dev.sk.compose.ui.base.movielist.MoviesListScreen
+import com.dev.sk.compose.ui.base.theme.MovieDetailsRoute
+import com.dev.sk.compose.ui.base.theme.MoviesListRoute
+import com.dev.sk.compose.ui.base.theme.MoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.reflect.typeOf
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            MoviesTheme {
+                Surface(color = Color.White) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = MoviesListRoute
+                    ) {
+                        composable<MoviesListRoute> {
+                            MoviesListScreen(
+                                onMovieClick = { movie ->
+                                    navController.navigate(
+                                        MovieDetailsRoute(movieDetails = movie)
+                                    )
+                                }
+                            )
+                        }
+
+                        composable<MovieDetailsRoute> (
+                            typeMap = mapOf(
+                                typeOf<Movie>() to CustomNavType.MovieType
+                            )
+                        ) {
+                            val arguments = it.toRoute<MovieDetailsRoute>()
+                            MovieDetailsScreen(
+                                navController = navController,
+                                movie = arguments.movieDetails
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
